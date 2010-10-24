746113eb8aa1949e4d953a41a52d82be7c365142
cyrilfeng@gmail.com <cyrilfeng@gmail.com@f61523e6-c8ec-672f-79a5-5881441e1a0c>
2010-10-24 08:48:58 +0000

    add i18n

---
+++ trunk/Redirector/i18n/i18n.js
@@ -0,0 +1,130 @@
+var i18nT_en = {
+	"navbar-title": "Redirector Settings",
+	"navbar-pref": "Preferences",
+	"navbar-mngrule": "Manage Rules",
+	"h1-pref": "Preferences",
+	"h3-lang": "Language",
+	"h3-amredir": "Auto/Manual Redirect",
+	"pref-autoredir": "Enable auto redirect",
+	"pref-manredir": "Enable page action",
+	"h1-mngrule": "Manage Rules",
+	"h3-rulelist": "Rule List",
+	"table-name": "Rule Name",
+	"table-white": "Whitelist",
+	"table-black": "Blacklist",
+	"table-substr": "RegExp/SubStr",
+	"table-repl": "Replacement",
+	"h3-manredir": "Manual Redirect",
+	"op-edit": "Current Rule",
+	"op-sixxs": "SixXS.org IPv6-to-IPv4 Proxy",
+	"op-zju6": "to4.zju6.edu.cn IPv6-to-IPv4 Proxy",
+	"op-googlecache": "Access Google Cache Text-only Version",
+	"lb-name": "Rule Name",
+	"lb-white": "Whitelist",
+	"lb-black": "Blacklist",
+	"lb-substr": "RegExp/SubStr",
+	"lb-repl": "Replacement",
+	"lb-testurl": "Test URL",
+};
+
+var i18nP_en = {
+	"pref-savebttn": "Save",
+	"pref-cancelbttn": "Cancel",
+	"bttns-add": "Add",
+	"bttns-edit": "Edit",
+	"bttns-del": "Delete",
+	"bttns-up": "Up",
+	"bttns-down": "Down",
+	"bttns-addToManList": "Add to Manual List",
+	"manredir-rm": "Remove form Manual List",
+	"edit-save": "Save",
+	"edit-discard": "Discard",
+};
+
+var i18nT_zhCN = {
+	"navbar-title": "Redirector 设置",
+	"navbar-pref": "首选项",
+	"navbar-mngrule": "管理规则",
+	"h1-pref": "首选项",
+	"pref-autoredir": "自动重定向",
+	"pref-manredir": "手动重定向",
+	"h1-mngrule": "管理规则",
+	"h3-lang": "语言",
+	"h3-amredir": "自动/手动重定向",
+	"h3-rulelist": "规则列表",
+	"table-name": "规则名称",
+	"table-white": "白名单",
+	"table-black": "黑名单",
+	"table-substr": "匹配子串",
+	"table-repl": "替换式",
+	"h3-manredir": "手动重定向",
+	"op-edit": "当前条目",
+	"op-sixxs": "在域名后添加sixxs.org",
+	"op-zju6": "在域名后添加to4.zju6.edu.cn",
+	"op-googlecache": "直接使用Google网页快照的纯文字版本",
+	"lb-name": "规则名称",
+	"lb-white": "白名单",
+	"lb-black": "黑名单",
+	"lb-substr": "子串",
+	"lb-repl": "替换式",
+	"lb-testurl": "测试URL",
+};
+
+var i18nP_zhCN = {
+	"pref-savebttn": "保存",
+	"pref-cancelbttn": "取消",
+	"bttns-add": "添加",
+	"bttns-edit": "编辑",
+	"bttns-del": "删除",
+	"bttns-up": "上移",
+	"bttns-down": "下移",
+	"bttns-addToManList": "添加到手动列表",
+	"manredir-rm": "从列表中删除",
+	"edit-save": "保存",
+	"edit-discard": "取消",
+}
+
+notif_en = {
+	"illegal_re": ["Error", "Illegal Regular Expression!"],
+	"value_blank": ["Error", "Value should not be blank!"],
+	"endless_loop": ["Error", "Endless loop!"],
+	"not_matched": ["Error", "Not matched!"],
+	"confirm_discard": "Changes are not saved, discard anyway?",
+};
+
+notif_zhCN = {
+	"illegal_re": ["错误", "非法的正则表达式！"],
+	"value_blank": ["错误", "输入不能为空！"],
+	"endless_loop": ["错误", "死循环！"],
+	"not_matched": ["错误", "规则不匹配！"],
+	"confirm_discard": "改动未保存，确认取消？",
+};
+
+function lang() {
+	var langTag;
+	selLang = localStorage.selLang;
+	if(!(selLang === undefined)) {
+		chooseLang.selectedIndex = eval(selLang);
+		switch(selLang) {
+		case "1":
+			langTag = "zh-CN";
+			break;
+		default:
+			langTag = "en";
+		}
+	} else {
+		langTag = navigator.language;
+	}
+
+	switch(langTag){
+	case "zh-CN":
+		this.i18nT = i18nT_zhCN;
+		this.i18nP = i18nP_zhCN;
+		this.notif = notif_zhCN;
+		break;
+	default:
+		this.i18nT = i18nT_en;
+		this.i18nP = i18nP_en;
+		this.notif = notif_en;
+	}
+}
---
+++ trunk/Redirector/js/linkTrans.js
@@ -0,0 +1,51 @@
+
+window.addEventListener("click", function(e) {
+	var link = e.srcElement;
+
+	while(!/^a$/i.test(link.tagName)) {
+		if(/^body$/i.test(link.tagName)) {
+			return;
+		}
+
+		link = link.parentElement;
+	}
+
+	if(link.target == "_blank" || link.target == "new" || link.rel == "external") {
+/*		if(link.href.match(/www\.google\.com(\.\w\w)?\/url\?/i) || link.href.match(/\/url\?q=/i)) {
+			linkUrl = decodeURIComponent(linkUrl);
+			linkUrl = link.href.replace(/^.+?(http:\/\/.+?)(&ei.+)?$/i, "$1");
+		} else if(link.href.match(/webcache\.googleusercontent\.com\/.+ct=clnk/i)) {
+			linkUrl = link.href.replace(/&cd=.+&/i, "&");
+			linkUrl = linkUrl.replace(/&ct=clnk/i, "&newwindow=1&strip=1");
+		} else {*/
+			linkUrl = link.href;
+//		}
+		e.preventDefault();
+		var port = chrome.extension.connect({name: "link"});
+		port.postMessage({url: linkUrl});
+	}
+}, false);
+
+/*chrome.extension.onConnect.addListener(function (port) {
+	port.onMessage.addListener(function(msg) {
+		if(port.name == "urlCached") {
+			chrome.tabs.getCurrent(function(tab) {
+				if(tab.Id != msg.tabId) {
+					return;
+				}
+			});
+
+			var port = chrome.extension.connect({name: "urlCached"});
+
+			alert(urlCached);
+			if(urlCached == "") {
+				urlCached = msg.urlCached;
+				port.postMessage({url: ""});
+			} else {
+				port.postMessage({url: urlCached);
+			}
+		}
+
+	});
+});
+*/
